05625e7185ac5f75f9fe7de2e1b12ea7b79ec132
Antonio Napolitano <nap@napaalm.xyz>
2021-03-04 17:06:06 +0100

    add argument parser

--- syncom.py
+++ syncom.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+import argparse
 import lxml.html as html
 import lxml.etree as etree
 import requests
@@ -8,6 +10,14 @@ from urllib.parse import urljoin
 FEED = "paste url here"
 
 def main():
+    # setup argument parser
+    parser = argparse.ArgumentParser(description="Semplice script per scaricare periodicamente i comunicati da nuvola.madisoft.it")
+    parser.add_argument("username", metavar="USER", type=str, help="nome utente per il registro")
+    parser.add_argument("password", metavar="PASS", type=str, help="password per il registro")
+
+    # parse arguments
+    args = parser.parse_args()
+
     with requests.Session() as sesh:
         # obtain csrf token from login page
         r = sesh.get("https://nuvola.madisoft.it/login")
