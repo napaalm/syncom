331b54545a3eb618580b768b6632bc3b055dc241
Antonio Napolitano <nap@napaalm.xyz>
2021-03-22 17:20:07 +0100

    retry every request until success

--- syncom.py
+++ syncom.py
@@ -11,9 +11,23 @@ import lxml.etree as etree
 import requests
 from urllib.parse import urljoin
 from datetime import datetime
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
 
 # regex that matches a comunicato's filename
 filename_regex = re.compile(r"^\d+[ _]*-.*\.pdf$")
+
+def requests_retry_session(backoff_factor=1, status_forcelist=(500, 502, 504)):
+    session = requests.Session()
+    retry = Retry(
+        total=None,
+        backoff_factor=backoff_factor,
+        status_forcelist=status_forcelist,
+    )
+    adapter = HTTPAdapter(max_retries=retry)
+    session.mount('http://', adapter)
+    session.mount('https://', adapter)
+    return session
 
 def login(sesh, username, password):
     # obtain csrf token from login page
@@ -50,7 +64,7 @@ def main():
     categories = {name:url for (name, url) in args.c}
 
     try:
-        with requests.Session() as sesh:
+        with requests_retry_session() as sesh:
             # perform login
             login(sesh, args.username, args.password)
 
